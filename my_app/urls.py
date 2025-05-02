5800926fae07335d97b2606db21aaae6bbf84bf0
Ana <anageguchadze22@gmail.com>
2025-05-02 17:01:43 +0400

    Add urls

---
+++ my_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, OrderViewSet, OrderItemViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'order-items', OrderItemViewSet, basename='order-item')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
