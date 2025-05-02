a8d3f7983166913c5e042be4d674363834886085
Ana <anageguchadze22@gmail.com>
2025-05-02 16:29:30 +0400

    Add ProductViewSet

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import Product, Order, OrderItem
+from .serializers import OrderItemSerializer, OrderSerializer, ProductSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Product.objects.all()
+        product = get_object_or_404(queryset, pk=pk)
+        serializer =  ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = ProductViewSet(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = {IsAuthenticated}
+        return [permission() for permission in permission_classes]
+
+
